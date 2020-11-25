6af98844edf5f02cf1d68e6d1d8d64ad5d2c9ce2
viktorx64 <viktor.sundqvist2@gmail.com>
2020-11-25 11:54:57 +0100

    Made The app start

---
+++ getInfo.js
@@ -0,0 +1,17 @@
+/*
+	Get Info from nobel nominies
+*/
+
+console.log("Program starting");
+var express = require('express');
+var router = express()
+
+router.get('/', function(req, res, next) {
+	res.send("It Works");
+	next();
+})
+
+router.listen(3000, function(){
+	console.log("Server Listening on port 3000");
+});
+module.exports = router;
