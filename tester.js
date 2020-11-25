8153c689dd81f18ad057a66762d313fad47d19d5
viktorx64 <viktor.sundqvist2@gmail.com>
2020-11-25 12:26:41 +0100

    added tester

---
+++ tester.js
@@ -0,0 +1,33 @@
+/*
+	Tester of this Api
+	It will test all the important functions
+	to test if they work properly
+*/
+
+//setting it up
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+var should = require("chai").should()
+
+const baseUrl = "http://127.0.0.1:3000";
+const CheckingData = {
+	"firstname" : "John",
+	"lastname" : "Doe",
+	"id" : 0,
+	"votes": 42,
+	"accomplished" : "Test candidate"
+};
+
+chai.use(chaiHttp);
+// testing starts here
+
+describe("Load Candidates", function() {
+	it("Get all Candidates", function(done) {
+		chai.request(baseUrl)
+		.get("/")
+		.end(function(err, res) {
+			res.should.have.status(200);
+			done();
+		})
+	});
+});
