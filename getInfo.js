819b23034aeb51f12167f5fea23891c75e7d171d
viktorx64 <viktor.sundqvist2@gmail.com>
2020-11-26 12:10:13 +0100

    some save test

--- getInfo.js
+++ getInfo.js
@@ -3,16 +3,20 @@
 */
 
 console.log("Program starting");
-var express = require('express');
-var router = express()
+const express = require('express');
+const router = express()
+const bodyParser = require('body-parser');
 var fs = require('fs');
 
 const dataPath = "./data/candidates.json";
+const dataPathVotes = "./data/votes.json";
 const Image = "./data/test.png";
 router.get('/tester', function(req, res, next) {
 	res.send("It Works");
 	next();
 });
+
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/', function(req,res, next) {
 	fs.readFile(dataPath, (err, data) => {
@@ -20,7 +24,7 @@ router.get('/', function(req,res, next) {
 			throw err;
 		}
 		//var nobleList = JSON.parse(data);
-		res.send("find data on '/allNobel'\n it returns JSON");
+		res.send("find data on '/allNobel'<br> it returns JSON");
 		next();
 	});
 });
@@ -32,6 +36,41 @@ router.get('/allNobel', function(req,res, next) {
 		}
 		//var nobleList = JSON.parse(data);
 		res.send(JSON.parse(data));
+		next();
+	});
+});
+
+//get All Votes
+router.get('/votes', function(req,res, next) {
+	fs.readFile(dataPathVotes, (err, data) => {
+		if (err) {
+			throw err;
+		}
+		//var nobleList = JSON.parse(data);
+		res.send(JSON.parse(data));
+		next();
+	});
+});
+
+//save Votes
+router.put('/vote', function(req,res, next) {
+	var nobleList;
+	var id = parseInt(req.body.id);
+	fs.readFile(dataPathVotes, (err, data) => {
+		if (err) {
+			throw err;
+		}
+		nobleList = JSON.parse(data);
+		nobleList[id].votes++;
+		
+		fs.writeFile(dataPathVotes, JSON.stringify(nobleList), (err) => {
+			if (err) {
+				throw err;
+			}
+			console.log("somebody voted!");
+		});
+		
+		res.send(nobleList);
 		next();
 	});
 });
