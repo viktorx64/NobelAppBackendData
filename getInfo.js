a169eb54b39408112fa2d28d0592d4e927cffce6
viktorx64 <viktor.sundqvist2@gmail.com>
2020-11-27 13:43:17 +0100

    heroku test

--- getInfo.js
+++ getInfo.js
@@ -73,6 +73,7 @@ router.get('/votes', function(req,res, next) {
 
 //save Votes
 router.put('/vote', function(req,res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	var nobleList;
 	var id = parseInt(req.body.id);
 	fs.readFile(dataPathVotes, (err, data) => {
