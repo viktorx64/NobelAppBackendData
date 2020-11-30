5843e724320bc6019ba8e9b382171db430e74671
viktorx64 <viktor.sundqvist2@gmail.com>
2020-11-30 15:26:13 +0100

    added content-type for cors

--- getInfo.js
+++ getInfo.js
@@ -21,6 +21,7 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(function (req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers: Content-Type');
 	next();
 });
 
