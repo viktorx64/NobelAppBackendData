1ff1c67b995a3e6fc10be0aa64389fd6603447fd
viktorx64 <viktor.sundqvist2@gmail.com>
2020-11-30 15:36:59 +0100

    heroku fix#2

--- getInfo.js
+++ getInfo.js
@@ -21,7 +21,7 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(function (req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Access-Control-Allow-Origin');
 	next();
 });
 
